83929119b8ae95abd00649a3c513d3d53564cc57
Steven Topazio <topazio.steven@gmail.com>
2020-11-24 21:11:13 +0000

    Automatically backed up by Learn

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,19 @@
+module Memorable
+  module ClassMethods 
+    
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    self.all.count
+  end
+  
+end
+
+module InstanceMethods 
+  def initialize 
+    self.class.all << self 
+  end 
+end 
+end 
